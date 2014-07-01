cd3b9341e5a9055c070df3c4ab1530dceb73b290
Pete Bachant <petebachant@gmail.com>
2014-06-30 21:48:47 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='pxl',
+    version='0.0.1',
+    author='Pete Bachant',
+    author_email='petebachant@gmail.com',
+    packages=['pxl'],
+    scripts=[],
+    url='https://github.com/petebachant/pxl.git',
+    license='LICENSE',
+    description='Extra functions built on NumPy, SciPy, pandas, matplotlib, etc.',
+    long_description=open('README.md').read()
+)
