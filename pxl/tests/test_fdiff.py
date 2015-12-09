8ab18651733fca8e96bbab5425305375946ea0c5
Pete Bachant <petebachant@gmail.com>
2015-12-09 11:08:10 -0500

    Update plotting for fdiff uncertainties test

--- pxl/tests/test_fdiff.py
+++ pxl/tests/test_fdiff.py
@@ -30,10 +30,15 @@ def test_second_order_diff():
 def test_second_order_diff_uncertainties():
     """Test that `second_order_diff` works with uncertainties."""
     # Create a non-equally spaced x vector
-    x = np.append(np.linspace(0, np.pi, 100),
-                  np.linspace(np.pi + 0.01, 2*np.pi, 400))
-    x_unc = unumpy.uarray(x, np.ones(len(x))*1e-4)
-    u = unumpy.uarray(np.sin(x), np.ones(len(x))*1e-3)
+    x = np.append(np.linspace(0, np.pi, 50),
+                  np.linspace(np.pi + 0.01, 2*np.pi, 100))
+    x_unc = unumpy.uarray(x, np.ones(len(x))*1e-3)
+    u = unumpy.uarray(np.sin(x), np.ones(len(x))*1e-2)
     dudx = second_order_diff(u, x)
     print(dudx[:5])
     print(dudx[-5:])
+    if plot:
+        plt.errorbar(x, unumpy.nominal_values(dudx), yerr=unumpy.std_devs(dudx),
+                     fmt="-o", lw=2, alpha=0.5)
+        plt.plot(x, np.cos(x), "--^", lw=2, alpha=0.5)
+        plt.show()
