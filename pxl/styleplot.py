d7d06cb74422e1ac0154741351fbecea080cfcc0
Pete Bachant <petebachant@gmail.com>
2016-02-11 12:11:57 -0500

    Whitespace fixes in obsolete functions

--- pxl/styleplot.py
+++ pxl/styleplot.py
@@ -7,25 +7,25 @@ import matplotlib.pyplot as plt
 
 
 def styleplot():
-    font = {"family":"serif","serif":"cmr10","size":23}
-    lines = {"markersize":9, "markeredgewidth":0.9}
-    legend = {"numpoints":1, "fontsize": "small"}
-    matplotlib.rc("text", usetex = True)
+    font = {"family": "serif", "serif": "cmr10", "size":23}
+    lines = {"markersize": 9, "markeredgewidth": 0.9}
+    legend = {"numpoints": 1, "fontsize": "small"}
+    matplotlib.rc("text", usetex=True)
     matplotlib.rc("font", **font)
     matplotlib.rc("lines", **lines)
     matplotlib.rc("legend", **legend)
-    matplotlib.rc("xtick", **{"major.pad":12})
+    matplotlib.rc("xtick", **{"major.pad": 12})
     plt.tight_layout()
 
 
 def setpltparams(fontsize=16, latex=True):
     if latex:
-        font = {"family" : "serif", "serif" : "cmr10", "size" : fontsize}
+        font = {"family": "serif", "serif": "cmr10", "size": fontsize}
     else:
         font = {"size" : fontsize}
-    lines = {"markersize" : 9, "markeredgewidth" : 1,
-             "linewidth" : 1.2}
-    legend = {"numpoints" : 1, "fontsize" : "small"}
+    lines = {"markersize": 9, "markeredgewidth": 1,
+             "linewidth": 1.2}
+    legend = {"numpoints": 1, "fontsize": "small"}
     matplotlib.rc("text", usetex=latex)
     matplotlib.rc("font", **font)
     matplotlib.rc("lines", **lines)
