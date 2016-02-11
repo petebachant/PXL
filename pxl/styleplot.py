1a4ea3a6cb4aa540c2cfdbabbc19f8f84a2c8f69
Pete Bachant <petebachant@gmail.com>
2016-02-11 12:10:12 -0500

    Add color_codes parameter to set_sns

--- pxl/styleplot.py
+++ pxl/styleplot.py
@@ -37,19 +37,21 @@ def set_default_fontsize(size=23):
     matplotlib.rc("font", size=size)
 
 
-def set_sns(style="white", context="paper", font_scale=1.5, rc={}):
-    """Set plot style using seaborn."""
+def set_sns(style="white", context="paper", font_scale=1.5, color_codes=True,
+            rc={}):
+    """Set default plot style using seaborn.
+
+    Font size is set to match the size of the tick labels, rather than the axes
+    labels.
+    """
     rcd = {"lines.markersize": 8, "lines.markeredgewidth": 1.25,
            "legend.fontsize": "small", "font.size": 12/1.5*font_scale,
            "legend.frameon": True, "axes.formatter.limits": (-5, 5),
            "axes.grid": True}
     rcd.update(rc)
-    rc = rcd
-    try:
-        import seaborn as sns
-        sns.set(style=style, context=context, font_scale=font_scale, rc=rc)
-    except ImportError:
-        print("Install Seaborn to use styleplot.set_sns")
+    import seaborn as sns
+    sns.set(style=style, context=context, font_scale=font_scale,
+            color_codes=color_codes, rc=rcd)
 
 
 def label_subplot(ax=None, x=0.5, y=-0.25, text="(a)", **kwargs):
--- pxl/tests/test_styleplot.py
+++ pxl/tests/test_styleplot.py
@@ -7,9 +7,11 @@ from pxl.styleplot import *
 def test_set_sns(**args):
     """Test the Seaborn plot styling."""
     set_sns(**args)
-    plt.plot([1, 2, 3], label="Test")
+    plt.plot(range(10), marker="o", mfc="none", mec="b", label="Unfilled")
+    plt.plot(range(1, 6), marker="s", color="r", label="Seaborn red")
     plt.xlabel(r"$T^{est}$")
     plt.ylabel("Test")
+    plt.text(x=5, y=1.5, s="Here's some text")
     plt.legend()
     plt.tight_layout()
     plt.show()
